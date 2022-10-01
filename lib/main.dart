64adc3f86df42a89da259521fc97daf07166f453
David Rios <david.rios@soytul.com>
2022-10-01 13:15:04 -0400

    Initial configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
+
+class NasaColors {
+  static const Color primaryColor = Color(0xff11122D);
+  static const Color accentColor = Color(0xffCC90CB);
+  static const Color primaryLightColor = Color(0xffffffff);
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return MaterialApp(
       title: 'Material App',
+      theme: ThemeData(
+        primaryColor: NasaColors.primaryColor,
+        accentColor: NasaColors.accentColor,
+        primaryColorLight: NasaColors.primaryLightColor,
+        scaffoldBackgroundColor: NasaColors.primaryColor,
+        textTheme: GoogleFonts.spaceMonoTextTheme(textTheme),
+      ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          centerTitle: true,
+          title: const Text(
+            'Nombre de la app',
+          ),
+          backgroundColor: Colors.transparent,
         ),
         body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+            child: Text(
+          'Hola Mundo',
+          style: TextStyle(color: Theme.of(context).primaryColorLight),
+        )),
       ),
     );
   }
