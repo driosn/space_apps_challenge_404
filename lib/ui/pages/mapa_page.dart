5a28272c05ecf2155220d63d79232229de88d17d
JohnnHidalgoCanaviri <johnn.hidalgo@inti.com.bo>
2022-10-02 00:03:08 -0400

    feat ref imports

--- lib/ui/pages/mapa_page.dart
+++ lib/ui/pages/mapa_page.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
-// import 'package:permission_handler/permission_handler.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:space_apps_404_name_not_found/utils/asset_provider.dart';
 import 'package:lottie/lottie.dart' as lt;
 
